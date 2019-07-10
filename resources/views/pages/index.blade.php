621a2b3bf50fb8c18d601c29c11125d17fa7f51d
marina-saad <marina.saad28@hotmail.com>
2019-07-10 23:27:43 +0200

    Update_home

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -87,69 +87,90 @@
 <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3 menu">
     <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
         <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
+            <h2 class="display-5">Welcome message</h2>
+            <p class="lead">When the receptionist end the guest's check-in, when the guest say "hey, Jenny" the assistant welcomes him</p>
         </div>
-
-        <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 250px; border-radius: 21px 21px 0 0;">
+            <img class="img" src="images\app\welcome.png">
+        </div>
     </div>
     <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
         <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
+            <h2 class="display-5">Tell me a joke</h2>
+            <p class="lead">"Retrieve random joke from local database
+                This local database will be updated every 20 mins with new jokes"</p>
         </div>
-        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        <div class="bg-dark shadow-sm mx-auto" style="width: 70%; height: 210px; border-radius: 21px 21px 0 0;">
+            <img class="img" src="images\app\joke.jpg">
+        </div>
     </div>
 </div>
 
 <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3 menu">
     <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
         <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
+            <h2 class="display-5">Morning text</h2>
+            <p class="lead">The mornig text sections provide 
+              news and/or delete any section ther guest didn't need it as morning text</p>
         </div>
-        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 250px; border-radius: 21px 21px 0 0;">
+            <img class="img" src="images\app\morning.jpg">
+        </div>
     </div>
     <div class="bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
         <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
+            <h2 class="display-5">Estimate trip time | provide location </h2>
+            <p class="lead">The smart assistant will send the location to our mob app and tell to the guest the time estimation for this trip</p>
         </div>
-        <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 250px; border-radius: 21px 21px 0 0;">
+            <img class="img" src="images\app\estimate.jpg">
+        </div>
+    </div>
+</div>
+
+<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3 menu">
+    <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+        <div class="my-3 p-3">
+            <h2 class="display-5">Hotel sounds playlist</h2>
+            <p class="lead">The smart assistant will get the names of those sounds from hotel database then get the sounds 
+              of those names from such music provider and play some tracks of them</p>
+        </div>
+        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 210px; border-radius: 21px 21px 0 0;">
+            <img class="img" src="images\app\music.jpg">
+        </div>
+    </div>
+    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+        <div class="my-3 py-3">
+            <h2 class="display-5">IOT controlling</h2>
+            <p class="lead">IOT server will received a specific command to control an IOT device 
+              but the guest could provide this command over voice to smart assistant hardware</p>
+        </div>
+        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 240px; border-radius: 21px 21px 0 0;">
+            <img class="img" src="images\app\IOT.jpg">
+        </div>
     </div>
 </div>
 
 <div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3 menu">
     <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
         <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
+            <h2 class="display-5">Emergency call </h2>
+            <p class="lead">The smart assistant will handle the emergency case by showing note. 
+              On desktop interface for hotel reception</p>
         </div>
-        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 200px; border-radius: 21px 21px 0 0;">
+            <img class="img" src="images\app\emergency.jpg">
+        </div>
     </div>
-    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+    <div class="bg-primary mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
         <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
+            <h2 class="display-5">Alarms</h2>
+            <p class="lead">The smart assistant will ring an alarm at this certain time by
+               playing an song from guest's choices</p>
         </div>
-        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-    </div>
-</div>
-
-<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3 menu">
-    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 p-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
+        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 200px; border-radius: 21px 21px 0 0;">
+            <img class="img" src="images\app\alarm.jpg">
         </div>
-        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-    </div>
-    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-        <div class="my-3 py-3">
-            <h2 class="display-5">Another headline</h2>
-            <p class="lead">And an even wittier subheading.</p>
-        </div>
-        <div class="bg-white shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
     </div>
 </div>
 
--- resources/views/pages/sessions.blade.php
+++ resources/views/pages/sessions.blade.php
@@ -14,13 +14,58 @@
         <li class="breadcrumb-item active" aria-current="page">sessions</li>
       </ol>
     </nav>
-    <div class="card-columns">
-      <div class="card">
+ {{--   <div class="card" style="background-color:ivory">
         <div class="card-body">
-          <h5 class="card-title">History</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <p>
+              <a class="btn btn-primary" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">hotel details</a>
+              <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">session details</button>
+              {{--<button class="btn btn-primary" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Toggle both elements</button>--}}
+     {{--       </p>
+            <div class="row">
+              <div class="col">
+                <div class="collapse multi-collapse" id="multiCollapseExample1">
+                  <div class="card card-body">
+                      <p>Hotel name</p>
+                      <p>Description</p>
+                      <p>Address</p>
+                      <p>Rate</p>
+                      <p>Country City</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <div class="collapse multi-collapse" id="multiCollapseExample2">
+                  <div class="card card-body">
+                   <p>Room ID</p>
+                   <p>Check in date</p>
+                   <p>Check out date</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          {{--<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>--}}
+       {{--   <div class="d-flex align-items-start flex-column bd-highlight mb-3" style="height: 200px;background-color:lightgreen">
+            <p>Feedback</p>
+            <p>Q1</p>
+            <p>A1</p>
+          </p>
+          </div>
         </div>
       </div>
     </div>
+    --}}
+    <div class="card">
+        <div class="row no-gutters">
+            <div class="col-auto">
+                <img src="images\app\hotel.jpg" class="img-fluid" alt="" style="width:100px">
+            </div>
+            <div class="col">
+                <div class="card-block px-2">
+                    <h4 class="card-title">Title</h4>
+                    <p class="card-text">Description</p>
+                    <a href="#" class="btn btn-primary">BUTTON</a>
+                </div>
+            </div>
+        </div>
+  </div>
 @endsection
